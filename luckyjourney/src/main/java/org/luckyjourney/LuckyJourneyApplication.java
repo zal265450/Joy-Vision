a4fca54ea58ea684d2639c52602482fc9091ec9a
Xhy <1416318023@qq.com>
2023-10-24 10:44:48 +0800

    init 初始化项目

---
+++ luckyjourney/src/main/java/org/luckyjourney/LuckyJourneyApplication.java
@@ -0,0 +1,13 @@
+package org.luckyjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyJourneyApplication.class, args);
+    }
+
+}
---
+++ luckyjourney/src/test/java/org/luckyjourney/LuckyJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.luckyjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
