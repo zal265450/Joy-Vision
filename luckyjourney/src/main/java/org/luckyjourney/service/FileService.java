f2cfeafb6e8c93e226e88810574adbf36a5bff3c
John <2823639170@qq.com>
2023-10-25 19:50:06 +0800

    feat: #完成上传policy接口

---
+++ luckyjourney/src/main/java/org/luckyjourney/service/FileService.java
@@ -0,0 +1,16 @@
+package org.luckyjourney.service;
+
+import org.luckyjourney.util.R;
+
+/**
+ * @author EVA
+ */
+public interface FileService {
+
+
+    /**
+     * kodo上传签名
+     * @return 返回签名
+     */
+    R policy();
+}
