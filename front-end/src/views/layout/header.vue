6d15fab221be698983e2a66ebe4f421c2cc61729
LiusCraft <liuscraft@qq.com>
2023-10-26 18:17:07 +0800

    修改头部和侧部导航样式

--- front-end/src/views/layout/header.vue
+++ front-end/src/views/layout/header.vue
@@ -1,24 +1,19 @@
 <template>
-    <v-app-bar color="blue" name="app-bar" :elevation="0" title="Luck jouney">
-      <div style="text-align: center;width: 80%;margin-top: 20px;">
-        <v-autocomplete
-        :items="items"
-        
-        style="width: 300px;margin: 0 auto; text-align: center;"
-        append-inner-icon="mdi-microphone"
-        auto-select-first
-        class="flex-full-width"
-        density="comfortable"
-        item-props
-        menu-icon=""
-        placeholder="搜索视频/用户"
-        prepend-inner-icon="mdi-magnify"
-        rounded
-        theme="light"
-      ></v-autocomplete>
-      </div>
-      <v-btn :elevation="2">登录</v-btn>
-      </v-app-bar>
+  <v-app-bar name="app-bar" :elevation="0">
+    <v-app-bar-title>幸运日</v-app-bar-title>
+    <v-card-subtitle>LUCK JOUNEY</v-card-subtitle>
+    <div style="text-align: center;width: 80%;">
+      <v-autocomplete :items="items" style="width: 300px;margin: 0 auto; text-align: center;" auto-select-first
+        class="flex-full-width" density="comfortable" item-props menu-icon="" placeholder="搜索视频/用户 (高级)"
+        prepend-inner-icon="mdi-magnify" rounded hide-details theme="light"></v-autocomplete>
+    </div>
+    <v-btn variant="elevated">登录</v-btn>
+  </v-app-bar>
 </template>
 <script setup>
 </script>
+<style scoped>
+::v-deep(.v-field__outline) {
+  --v-field-border-width: 0px
+}
+</style>
