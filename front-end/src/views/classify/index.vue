f629cc56d1dda7090b5ff7d055ba3f440d67bc05
LiusCraft <liuscraft@qq.com>
2023-10-27 15:53:04 +0800

    新增分类页样式，微调首页样式

---
+++ front-end/src/views/classify/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <VContainer>
+        <!-- 用户订阅的分类，没有登录则不显示 -->
+        <MyClassify/>
+        <!-- 系统分类 -->
+        <AllClassify class="mt-8"/>
+    </VContainer>
+</template>
+<script setup>
+import AllClassify from './all.vue';
+import MyClassify from './my.vue';
+
+</script>
