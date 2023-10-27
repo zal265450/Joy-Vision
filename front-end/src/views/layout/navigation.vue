bcfe91440475eb0f7c8dc48adef673313aa4d459
LiusCraft <liuscraft@qq.com>
2023-10-27 15:44:20 +0800

    微调导航样式和新增菜单项

--- front-end/src/views/layout/navigation.vue
+++ front-end/src/views/layout/navigation.vue
@@ -1,23 +1,23 @@
 <template>
-    <v-navigation-drawer
-        >
-          <v-list>
-            <v-list-item
-              prepend-avatar="/logo.png"
-              title="七牛云-小虎"
-              subtitle="xiaohu@qiniu.com"
-            ></v-list-item>
-          </v-list>
-  
-          <v-divider></v-divider>
-  
-          <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-home" title="首页" value="home"></v-list-item>
-            <v-list-item prepend-icon="mdi-video" title="推荐视频(高级)" value="video"></v-list-item>
-            <v-list-item prepend-icon="mdi-label-multiple" title="视频分类" value="fire"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-multiple" title="粉丝/关注(高级)" value="shared"></v-list-item>
-            <v-list-item prepend-icon="mdi-star" title="收藏夹(高级)" value="starred"></v-list-item>
-            
-          </v-list>
-        </v-navigation-drawer>
-  </template>
+  <v-navigation-drawer color="#252632">
+    <v-list>
+      <v-list-item prepend-avatar="/logo.png" title="七牛云-小虎" subtitle="xiaohu@qiniu.com"></v-list-item>
+    </v-list>
+
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home" title="首页" to="/"></v-list-item>
+      <v-list-item prepend-icon="mdi-video" title="推荐视频(高级)" to="/video"></v-list-item>
+      <v-list-item prepend-icon="mdi-label-multiple" title="视频分类" to="/classify"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-multiple" title="粉丝/关注(高级)" to="/friends"></v-list-item>
+      <v-list-item prepend-icon="mdi-star" title="收藏夹(高级)" to="/favorites"></v-list-item>
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<style lang="scss" scoped>
+.v-navigation-drawer{
+  border: none !important;
+
+}
+</style>
