b6e340c7a6957371d5455d6f6b3d7d3c36583eef
LiusCraft <liuscraft@qq.com>
2023-11-07 22:37:04 +0800

    fix: 修复分类丢失

--- front-end/src/views/user/classify/all.vue
+++ front-end/src/views/user/classify/all.vue
@@ -4,9 +4,8 @@
         <VCardSubtitle style="display: inline;">所有视频分类</VCardSubtitle>
         <VDivider />
         <v-chip-group class="ma-2">
-            <v-chip v-for="(item, index) in classifyDataList" :key="index" :size="'large'" closable
-                :close-icon="item.used ? 'mdi-close-circle' : 'mdi-plus-circle'"
-                @click:close="closeEvent(item.id, !item.used)">
+            <v-chip v-for="(item, index) in classifyData" :key="index" :size="'large'"
+                @click="closeEvent(item, true)">
                 <template #prepend>
                     <VAvatar :image="item.image" :icon="item.icon || 'mdi-file-document-alert-outline'" start></VAvatar>
                 </template>
@@ -16,34 +15,15 @@
     </div>
 </template>
 <script setup>
-import { computed } from 'vue';
 
 const props = defineProps({
     classifyData: {
         type: Array,
         default: []
     },
-    used: {
-        type: Array,
-        default: [
-
-        ]
-    },
     closeEvent: {
         type: Function,
         default: () => { }
     }
-})
-const classifyDataList = computed(() => {
-    return props.used.reduce((acc, cur) => {
-        const target = acc.find(e => e.id === cur.id);
-        if (target) {
-            Object.assign(target, cur);
-            target.used = true
-        } else {
-            acc.push(cur);
-        }
-        return acc;
-    }, props.classifyData)
 })
 </script>
--- front-end/src/views/user/classify/my.vue
+++ front-end/src/views/user/classify/my.vue
@@ -5,7 +5,7 @@
         <VDivider />
         <v-chip-group class="ma-2">
             <v-chip v-for="(item, index) in classifyData" :key="index" :size="'large'" closable
-                @click:close="closeEvent(item.id)">
+                @click:close="closeEvent(index)">
                 <template #prepend>
                     <VAvatar :image="item.image" :icon="item.icon || 'mdi-file-document-alert-outline'" start></VAvatar>
                 </template>
