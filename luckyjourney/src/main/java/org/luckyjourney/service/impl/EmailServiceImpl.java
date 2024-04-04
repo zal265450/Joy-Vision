adaf8891458a18276a3ab8f59b37ef73af6ad001
LiusCraft <meliuscraft@gmail.com>
2024-04-04 19:52:18 +0800

    fix(email): 修正邮件服务的from地址根据配置文件的mail_username决定

--- luckyjourney/src/main/java/org/luckyjourney/service/impl/EmailServiceImpl.java
+++ luckyjourney/src/main/java/org/luckyjourney/service/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package org.luckyjourney.service.impl;
 import org.luckyjourney.service.EmailService;
 import org.luckyjourney.util.RedisCacheUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -24,13 +25,16 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    String fromName;
+
 
 
     @Override
     @Async
     public void send(String email, String context) {
         simpleMailMessage.setSubject("幸运日");
-        simpleMailMessage.setFrom("1416318023@qq.com");
+        simpleMailMessage.setFrom(fromName);
         simpleMailMessage.setTo(email);
         simpleMailMessage.setText(context);
         javaMailSender.send(simpleMailMessage);
